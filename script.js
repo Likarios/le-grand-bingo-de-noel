9f98d966cf0b4ef61d9bc9471785c6a9fcb9e730
Likarios <likarios.lebeks@gmail.com>
2021-12-25 00:32:36 +0100

    Points working

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 var inputs = document.getElementsByTagName("input");
 var inputsById = {};
+var points = 0;
 //localStorage.clear();
 load();
 
 function load() {
     inputsById = JSON.parse(localStorage.getItem("save"));
+    points = 0;
     if(inputsById == null){
         return;
     }
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].checked = inputsById[i];
+        if(parseInt(inputs[i].dataset.points) * 0 == 0 && inputs[i].checked){
+            points += parseInt(inputs[i].dataset.points);
+        }
     }
+    updatePointsSpan();
 }
 
 function save() {
-    var inputsById = {};
+    inputsById = {};
+    points = 0;
     for (var i = 0; i < inputs.length; i++) {
         inputsById[i] = inputs[i].checked;
+        if(parseInt(inputs[i].dataset.points) * 0 == 0 && inputs[i].checked){
+            points += parseInt(inputs[i].dataset.points);
+        }
     }
     localStorage.setItem("save", JSON.stringify(inputsById));
+    updatePointsSpan();
+}
+
+function updatePointsSpan() {
+    document.getElementById("point").innerHTML = points;
 }
